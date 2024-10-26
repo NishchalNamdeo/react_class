8fb8e5b67029ada2cdc91282102177de39505e58
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2024-10-26 18:36:55 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,7 @@ const App = () => {
         <div className=" border-t-2 w-screen h-screen bg-zinc-800 flex  items-center flex-col">
             <div className="mt-[7%] w-[25%] h-[20%] border rounded-3xl flex justify-around items-center">
                 <div className="text-yellow-100">
+                    <h1>hwllo</h1>
                     <h1 className="text-3xl font-bold">LETS TODO</h1>
                     <p>Keeps doing things</p>
                 </div>
--- src/components/Userdetails.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Userdetails = () => {
-  return (
-    <div>UserDetails</div>
-  )
-}
-
-export default Userdetails
